31c4d7dcb43b0f2e551e76eff30fc55ed44ca79e
MaCoZu <marcoz@posteo.de>
2025-04-22 15:41:06 +0200

    devcontainer

--- tina/config.ts
+++ tina/config.ts
@@ -1,68 +1,72 @@
-import { defineConfig } from "tinacms";
+import { defineConfig, LocalAuthProvider } from "tinacms";
+import {
+    UsernamePasswordAuthJSProvider,
+    TinaUserCollection,
+} from "tinacms-authjs/dist/tinacms";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
+
 
 export default defineConfig({
-  branch,
+    branch: process.env.GITHUB_BRANCH || 'main',
+    clientId: process.env.TINA_CLIENT_ID,
+    token: process.env.TINA_TOKEN,
+    contentApiUrlOverride: '/api/tina/gql',
+    // authProvider: 'authjs',
+    authProvider: isLocal
+        ? new LocalAuthProvider()
+        : new UsernamePasswordAuthJSProvider(),
 
-  // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
-  token: process.env.TINA_TOKEN,
 
-  build: {
-    outputFolder: "admin",
-    publicFolder: "public",
-  },
-  media: {
-    tina: {
-      mediaRoot: "",
-      publicFolder: "public/images/",
+    build: {
+        outputFolder: "admin",
+        publicFolder: "public",
     },
-  },
-  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
-  schema: {
-    collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "src/content/news",
-        fields: [
-          {
-            name: "title",
-            label: "Title",
-            type: "string",
-            isTitle: true,
-            required: true,
-          },
-          {
-            name: 'pubDate',
-            label: 'Publication Date',
-            type: 'datetime',
-          },
-          {
-            name: 'updatedDate',
-            label: 'Update Date',
-            type: 'datetime',
-          },
-          {
-            name: 'thumbnail',
-            label: 'Image',
-            type: 'image',
-          },
-          {
-            name: "body",
-            label: "Body",
-            type: "rich-text",
-            isBody: true,
-          },
+    media: {
+        tina: {
+            mediaRoot: "",
+            publicFolder: "public/images/",
+            static: true,
+        },
+    },
+    // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+    schema: {
+        collections: [
+            {
+                name: "post",
+                label: "Posts",
+                path: "src/content/news",
+                fields: [
+                    {
+                        name: "title",
+                        label: "Title",
+                        type: "string",
+                        isTitle: true,
+                        required: true,
+                    },
+                    {
+                        name: 'pubDate',
+                        label: 'Publication Date',
+                        type: 'datetime',
+                    },
+                    {
+                        name: 'updatedDate',
+                        label: 'Update Date',
+                        type: 'datetime',
+                    },
+                    {
+                        name: 'thumbnail',
+                        label: 'Image',
+                        type: 'image',
+                    },
+                    {
+                        name: "body",
+                        label: "Body",
+                        type: "rich-text",
+                        isBody: true,
+                    },
+                ],
+            },
         ],
-      },
-    ],
-  },
+    },
 });
