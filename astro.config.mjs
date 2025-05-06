8ba0d6bfa3d585a5f278395320c7be3bf6367e58
MaCoZu <marcoz@posteo.de>
2025-05-06 11:23:51 +0200

    alpine menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), alpinejs()],
 
   vite: {
     plugins: [tailwindcss()],
